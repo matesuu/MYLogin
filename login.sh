b592efdb2ca41db962598ef451a1f47cbc6d9c75
matesuu <74740179+matesuu@users.noreply.github.com>
2024-10-15 18:09:10 -0700

    Update login.sh

--- login.sh
+++ login.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-python3 -m "login"
+python3 -m login
+
+pycache="__pycache__"
+
+if [ -d "$pycache" ]; then
+    rm -r "$pycache"
+    echo "deleted __pycache"
+
+else
+    exit 1
+
+fi
+
+exit 0
